de7c30d59105fc88427c1d09ce37f9aab49fa36f
Pous17 <116793451+Pous17@users.noreply.github.com>
2023-05-24 10:49:14 +0200

    code: input helper function & book availability display refactored

--- src/db_mods/borrow_book.rs
+++ src/db_mods/borrow_book.rs
@@ -7,56 +7,36 @@ pub fn borrow_book() {
     let lists = fetch();
     let book_list = lists.0;
     let user_list = lists.1;
-    let mut borrowed_books = Vec::<bool>::new();
-
     let connection = &mut connection();
 
     // Available book list
     println!("\nList of currently available books");
     println!("--------------");
-    for book in &book_list {
-        if !book.borrowed {
+    let available_books: Vec<_> = book_list
+        .iter()
+        .filter(|book| !book.borrowed)
+        .map(|book| {
             println!("{} | id: {}", book.name, book.id);
-        }
-        borrowed_books.push(book.borrowed);
-    }
+            book.borrowed
+        })
+        .collect();
 
-    if borrowed_books.iter().all(|&x| x == true) {
+    if available_books.is_empty() {
         println!("There is no currently available book\n");
-        return
+        return;
     }
 
     loop {
         println!("\nr_bilio > borrow book > ");
-        print!("Id of the book you want to borrow: ");
-        stdout().flush().unwrap();
 
-        // String input
-        let mut book_input = String::new();
-        stdin().read_line(&mut book_input).unwrap();
+        let book_id = input_i32("Id of the book you want to borrow: ");
+        let user_id = input_i32("Your user id: ");
 
-        // i32 parsing
-        let book_id = book_input.trim_end().parse::<i32>().unwrap_or(-1);
-
-        print!("Your user id: ");
-        stdout().flush().unwrap();
-
-        // String input
-        let mut user_input = String::new();
-        stdin().read_line(&mut user_input).unwrap();
-
-        // i32 parsing
-        let user_id = user_input.trim_end().parse::<i32>().unwrap_or(-1);
-
-        // Date with conversion to ISO 8601
         let date = Local::now();
-        let r_date = date + Duration::days(7);
+        let return_date = date + Duration::days(7);
 
-        let str_date = date.format("%Y-%m-%d").to_string();
-        let borrow_date = str_date.trim();
-
-        let r_str_date = r_date.format("%Y-%m-%d").to_string();
-        let return_date = r_str_date.trim();
+        let borrow_date = date.format("%Y-%m-%d").to_string();
+        let return_date = return_date.format("%Y-%m-%d").to_string();
 
         if book_id == -1 || user_id == -1 {
             println!("Enter a valid number");
@@ -65,16 +45,14 @@ pub fn borrow_book() {
                 if user.score > 0 {
                     if let Some(book) = book_list.iter().find(|x| x.id == book_id) {
                         if book.borrowed {
-                            println!("This book is not available");     
+                            println!("This book is not available");
                         } else {
-                            // call borrow function
                             let borrow = create_borrow(connection, &user_id, &book_id, &borrow_date);
                             println!("You borrowed {}, the borrow id is {}", book.name, borrow.id);
                             println!("You can borrow books up to 7 days, therefore, this book shall be returned on {}", return_date);
-        
-                            // change book availability status
+
                             borrow_status(connection, &book_id, &true);
-                            return                   
+                            return;
                         }
                     } else {
                         println!("There is no corresponding book to the id you provided");
@@ -87,4 +65,13 @@ pub fn borrow_book() {
             }
         }
     }
+}
+
+fn input_i32(prompt: &str) -> i32 {
+    print!("{}", prompt);
+    stdout().flush().unwrap();
+
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwrap_or(-1)
 }
