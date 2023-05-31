76cb17b60c6daf0eeb9f6d209f3e562068e543dc
Pous17 <116793451+Pous17@users.noreply.github.com>
2023-05-31 23:31:22 +0200

    drop migration fix

--- migrations/2023-04-25-140431_create_user/down.sql
+++ migrations/2023-04-25-140431_create_user/down.sql
@@ -1,5 +1,10 @@
-DROP TABLE borrows;
-DROP TABLE users;
-DROP TABLE books;
-DROP TABLE authors;
-DROP TABLE employees;
+-- Drop the foreign key constraint for `borrows` table
+ALTER TABLE borrows DROP CONSTRAINT IF EXISTS borrows_user_id_fkey;
+ALTER TABLE borrows DROP CONSTRAINT IF EXISTS borrows_book_id_fkey;
+
+-- Drop the tables in the reverse order
+DROP TABLE IF EXISTS borrows;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS employees;
